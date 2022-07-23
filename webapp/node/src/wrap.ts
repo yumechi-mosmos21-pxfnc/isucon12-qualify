794a735c0e1b807918e0f4e50df834ea843eddf4
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2022-07-23 13:54:28 +0900

    Revert "ファイル分割2"

--- webapp/node/src/wrap.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction, RequestHandler } from 'express'
-
-// see: https://expressjs.com/en/advanced/best-practice-performance.html#handle-exceptions-properly
-export const wrap =
-  (fn: (req: Request, res: Response, next: NextFunction) => Promise<Response | void>): RequestHandler =>
-    (req, res, next) =>
-      fn(req, res, next).catch(next)
