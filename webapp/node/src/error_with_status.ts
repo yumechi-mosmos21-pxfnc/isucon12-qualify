8203cb5804bca1eaeb95a427d4cc2e4e2a94faa5
mosmos21 <dev@mosmos21.net>
2022-07-23 13:59:08 +0900

    mainを戻す

--- webapp/node/src/error_with_status.ts
+++
@@ -1,9 +0,0 @@
-// カスタムエラーハンドラにステータスコード拾ってもらうエラー型
-export class ErrorWithStatus extends Error {
-  public status: number
-  constructor(status: number, message: string) {
-    super(message)
-    this.name = new.target.name
-    this.status = status
-  }
-}
